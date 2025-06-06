5363e951e37017d0784f9d05cc789ecb0d18716e
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-06 15:08:18 +0300

    edit container.tsx

--- app/components/Container/Container.tsx
+++ app/components/Container/Container.tsx
@@ -1,7 +1,11 @@
+import { ImageLoader } from "./ImageLoader/ImageLoader";
+
 export function Container() {
     return <div className="container">
         <div className="container__status-bar">
             <div className="container__status-bar-filler"></div>
         </div>
+
+        <ImageLoader></ImageLoader>
     </div>;
 }
