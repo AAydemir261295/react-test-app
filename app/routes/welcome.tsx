e4b09a6ac5718d94d134c1d13302818b378b15c3
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-07 19:40:17 +0300

    edit welcome route

--- app/routes/welcome.tsx
+++ app/routes/welcome.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Welcome() {
     return <div className="btn-container">
         <Link to="/testing">
-            <button className="btn-container__btn nostyle-btn" >Начать тест</button>
+            <button className="btn-container__btn btn nostyle-btn" >Начать тест</button>
         </Link>
     </div>;
 }
