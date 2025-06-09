0dcb2e9f2489bc0ed818c109177518f3397a743d
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-09 11:41:39 +0300

    redit imageLoaderComponent

--- app/components/ImageLoader/ImageLoaderComponent.tsx
+++ app/components/ImageLoader/ImageLoaderComponent.tsx
@@ -1,7 +1,9 @@
 import { useRef, useState, type RefObject } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export function ImageLoaderComponent() {
+    let navigate = useNavigate();
+
     var descriptions = ["Дом, дерево, человек", "Несуществующее животное", "Автопортрет"];
 
     var items = [];
@@ -11,6 +13,7 @@ export function ImageLoaderComponent() {
 
     var icons = { upload: "/assets/icons/upload-icon.svg", refresh: "/assets/icons/refresh-icon.svg" }
 
+    var uploadCount = 0;
 
     function onChange(e: React.ChangeEvent<HTMLInputElement>, idx: number, previewEle: HTMLImageElement, switchIcon: HTMLImageElement) {
         if (!e.target.files) {
@@ -23,10 +26,11 @@ export function ImageLoaderComponent() {
         reader.onloadend = function (e) {
             previewEle.src = reader.result == null ? "" : reader.result as string;
             switchIcon.src = icons.refresh;
+            ++uploadCount;
         };
     }
 
-    function refreshImg(previewEle: HTMLImageElement, switchIcon: HTMLImageElement) {
+    function refreshImg(previewEle: HTMLImageElement) {
         previewEle.src = "";
     }
 
@@ -38,7 +42,7 @@ export function ImageLoaderComponent() {
                 <figure className="upload-list__upload-wrapper">
                     <img ref={(ele) => { previewRefs.current[q] = ele; }} className="upload-list__upload-preview" src="null" alt="" />
                     <label className="upload-list__input-label" htmlFor={`fileInput${q}`}>
-                        <img ref={(ele1) => { uploadShareIconRefs.current[q] = ele1 }} src={icons.upload} className="upload-list__upload-icon" width={64} height={64} alt={`Загрузите фотографию: ${description}`} onClick={() => { refreshImg(previewRefs.current[q], uploadShareIconRefs.current[q]) }} />
+                        <img ref={(ele1) => { uploadShareIconRefs.current[q] = ele1 }} src={icons.upload} className="upload-list__upload-icon" width={64} height={64} alt={`Загрузите фотографию: ${description}`} onClick={() => { refreshImg(previewRefs.current[q]) }} />
                     </label>
                     <input onChange={(e) => onChange(e, q, previewRefs.current[q], uploadShareIconRefs.current[q])} className="upload-list__upload-input" id={`fileInput${q}`} type="file" />
                     <figcaption className="upload-list__upload-description">{description}</figcaption>
@@ -50,8 +54,11 @@ export function ImageLoaderComponent() {
 
 
     return <div className="image-loader">
-        <header className="image-loader__header"> <h1 className="image-loader__header-text">Загрузите фотографии рисунков</h1> </header>
-        <span className="image-loader__info-text">Допустимые форматы файлов: jpg, jpeg, png, pdf. Размер не более 5 Мб</span>
+        <header className="image-loader__header">
+            <h1 className="image-loader__header-title">Загрузите фотографии рисунков</h1>
+            <span className="image-loader__header-info-text">Допустимые форматы файлов: jpg, jpeg, png, pdf. Размер не более 5 Мб</span>
+        </header>
+
         <form className="test" action="">
             <ul className="nostyle-list upload-list">
                 {items}
@@ -59,9 +66,7 @@ export function ImageLoaderComponent() {
         </form>
         <footer className="image-loader__footer">
             <span className="stage">Шаг 1/3</span>
-            <Link to="/testing/2">
-                <button className="image-loader__footer-btn btn btn--disabled nostyle-btn">Далее</button>
-            </Link>
+            <button className="image-loader__footer-btn btn btn--disabled nostyle-btn" onClick={() => { if (uploadCount >= 3) { navigate("/testing/2", { state: { previous: "/testing/1" } }) } }}>Далее</button>
         </footer>
     </div>
 }
