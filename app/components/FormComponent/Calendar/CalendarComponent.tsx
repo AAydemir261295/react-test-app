201b4f6ca72b22b27fe32d00dd27df2ce48ae69b
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-09 12:40:14 +0300

    add CalendarComponent.tsx

---
+++ app/components/FormComponent/Calendar/CalendarComponent.tsx
@@ -0,0 +1,23 @@
+
+
+export function Calendar() {
+    return <div className="calendar">
+        <header>
+            <h3>Month Year</h3>
+        </header>
+
+        <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <div>
+
+        </div>
+    </div>;
+}
