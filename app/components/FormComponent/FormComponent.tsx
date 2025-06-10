1f82ad54fe2c39c699cc59189fb5387ba1693610
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-10 18:23:15 +0300

    edit FormComponent.tsx

--- app/components/FormComponent/FormComponent.tsx
+++ app/components/FormComponent/FormComponent.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { CalendarComponent } from "./Calendar/CalendarComponent";
+import { useNavigate } from "react-router";
 
 const chapters = [
     {
@@ -9,7 +10,8 @@ const chapters = [
             "Ребенок часто выражает радость и удовольствие",
             "Ребенок часто грустит или плачет без видимой причин",
             "Ребенок часто грустит или плачет без видимой причин",
-        ]
+        ],
+        names: ['radosti', "grusti"],
     },
     {
         header: "Раздел 2. Социальное взаимодействие",
@@ -18,7 +20,9 @@ const chapters = [
             "Ребенок легко заводит друзей",
             "Ребенок предпочитает играть один, а не с другими детьми",
             "Ребенок предпочитает играть один, а не с другими детьми",
-        ]
+        ],
+        names: ["friends", "alone"],
+
     },
     {
         header: "Раздел 3. Саморегуляция и поведение",
@@ -27,7 +31,9 @@ const chapters = [
             "Ребенок умеет следовать правилам и инструкциям",
             "Ребенку трудно контролировать свои импульсы",
             "Ребенку трудно контролировать свои импульсы",
-        ]
+        ],
+        names: ["free", "dangerous"],
+
     },
     {
         header: "Раздел 4. Самооценка и уверенность в себе",
@@ -36,15 +42,19 @@ const chapters = [
             "Ребенок уверен с своих силах и способностях",
             "Ребенок часто сомневается в себе",
             "Ребенок часто сомневается в себе",
-        ]
+        ],
+        names: ["confidence", "uncertainty"],
+
     }
 ]
 
 
 export function FormComponent() {
-
+    var navigate = useNavigate();
     var [date, setDate] = useState(new Date(Date.now()));
     var [isVisible, setIsVisible] = useState("calendar--invisible");
+    // var [form, setForm] = useState({});
+    var formRef = useRef(null);
 
     useEffect(() => {
         var calendar = document.querySelector(".calendar");
@@ -70,24 +80,20 @@ export function FormComponent() {
     function temp() {
         for (let q = 0; q < chapters.length; q++) {
             const chapter = chapters[q];
-            console.log(chapter);
+
             items.push(<li className="form__questions-item" key={q}>
                 <header className="form__chapter">
                     <h5 className="form__chapter-text">{chapter.header}</h5>
                 </header>
-
                 {chapter.questions.map((v, idx) =>
-                    // form__fieldset form__chapter-emotions
                     <fieldset key={idx} className="four-fieldset form__radios nostyle-fieldset">
                         <legend className="four-fieldset__legend">{v}</legend>
 
-
-                        {/* form__radio-container form__radio-container-emotions */}
                         <div className="four-fieldset__question">
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="very-rare" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="very-rare" name={idx > 1 ? chapter.names[1] + idx : chapter.names[0] + idx} />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Очень редко</span>
                                 </label>
@@ -95,7 +101,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="rare" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="rare" name={idx > 1 ? chapter.names[1] + idx : chapter.names[0] + idx} />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Редко</span>
                                 </label>
@@ -103,7 +109,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="sometimes" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="sometimes" name={idx > 1 ? chapter.names[1] + idx : chapter.names[0] + idx} />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Иногда</span>
                                 </label>
@@ -111,7 +117,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="often" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="often" name={idx > 1 ? chapter.names[1] + idx : chapter.names[0] + idx} />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Часто</span>
                                 </label>
@@ -119,7 +125,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="always" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="always" name={idx > 1 ? chapter.names[1] + idx : chapter.names[0] + idx} />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Всегда</span>
                                 </label>
@@ -142,16 +148,16 @@ export function FormComponent() {
             <h3 className="form__header-text">Общая информация о ребенке</h3>
         </header>
 
-        <form action="">
+        <form ref={formRef}>
 
             <div className="form__child-name-container">
                 <label className="form__child-name-label" htmlFor="child-name">Имя ребенка</label>
-                <input className="nostyle-input input" type="text" id="child-name" />
+                <input className="nostyle-input input" type="text" id="child-name" name="child-name" />
             </div>
 
             <div className="form__date-input-container">
                 <label className="form__date-input-label">Дата рождения ребенка</label>
-                <input className="form__date-input nostyle-input input" type="text" onClick={() => showCalendar()} value={date.toLocaleDateString("RU", { day: "2-digit", month: "2-digit", year: "numeric" })} readOnly />
+                <input className="form__date-input nostyle-input input" name="date" type="text" onClick={() => showCalendar()} value={date.toLocaleDateString("RU", { day: "2-digit", month: "2-digit", year: "numeric" })} readOnly />
                 <CalendarComponent className="form__date-input-calendar" today={date} updateInput={setDate} showCalendar={isVisible}></CalendarComponent>
             </div>
 
@@ -180,7 +186,7 @@ export function FormComponent() {
 
             <div className="form__parent-name-container">
                 <label className="form__parent-name-label" htmlFor="parent-name">Имя родителя, заполняющего анкету</label>
-                <input className="form__parent-name-input input" type="text" id="parent-name" />
+                <input className="form__parent-name-input input" type="text" id="parent-name" name="parent-name" />
             </div>
 
             <div className="form__notice-container">
@@ -195,7 +201,7 @@ export function FormComponent() {
             </div>
 
             <ul className="form__questions nostyle-list">
-                {/* {items} */}
+                {items}
 
                 <li>
                     <header className="form__chapter">
@@ -208,7 +214,7 @@ export function FormComponent() {
                         <div className="four-fieldset__question">
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="very-rare" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="very-rare" name="child-emotions" />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Очень редко</span>
                                 </label>
@@ -216,7 +222,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="rare" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="rare" name="child-emotions" />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Редко</span>
                                 </label>
@@ -224,7 +230,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="sometimes" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="sometimes" name="child-emotions" />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Иногда</span>
                                 </label>
@@ -232,7 +238,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="often" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="often" name="child-emotions" />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Часто</span>
                                 </label>
@@ -240,7 +246,7 @@ export function FormComponent() {
 
                             <div >
                                 <label className="radio-container" >
-                                    <input className="radio-container__input" type="radio" id="always" name="drone" />
+                                    <input className="radio-container__input" type="radio" id="always" name="child-emotions" />
                                     <span className="radio-container__checkmark"></span>
                                     <span className="radio-container__text">Всегда</span>
                                 </label>
@@ -252,30 +258,32 @@ export function FormComponent() {
                     <fieldset className="four-fieldset nostyle-fieldset">
                         <div className="four-fieldset__textarea-container">
                             <label className="four-fieldset__textarea-label" htmlFor="more">Есть ли у вашего ребенка какие-либо особенности развития или поведения о которых Вы хотели бы сообщить дополнительно?</label>
-                            <textarea className="textarea" name="" id="more"></textarea>
+                            <textarea className="textarea" name="peculiarities" id="more"></textarea>
                         </div>
                         <div className="four-fieldset__textarea-container">
                             <label className="four-fieldset__textarea-label" htmlFor="powers">Какие, на Ваш взгляд, сильные стороны и таланты есть у Вашего ребенка?</label>
-                            <textarea className="textarea" name="" id="powers"></textarea>
+                            <textarea className="textarea" name="talents" id="powers"></textarea>
                         </div>
                         <div className="four-fieldset__textarea-container">
                             <label className="four-fieldset__textarea-label" htmlFor="attention">Какие, на Ваш взгляд, области требуют особого внимания и развития у Вашего ребенка?</label>
-                            <textarea className="textarea" name="" id="attention"></textarea>
+                            <textarea className="textarea" name="attention" id="attention"></textarea>
                         </div>
                         <div className="four-fieldset__textarea-container">
                             <label className="four-fieldset__textarea-label">Обращались ли Вы ранее к специалистам (психологу, неврологу, логопеду) по поводу развития или поведения Вашего ребенка?</label>
-                            <textarea className="textarea" name="" id="doctors"></textarea>
+                            <textarea className="textarea" name="psy" id="doctors"></textarea>
                         </div>
                     </fieldset>
                 </li>
             </ul>
+
+
         </form>
         <footer className="form-footer">
             <span className="form-footer__stage">Шаг 2/3</span>
 
-            <div>
-                <button className="form-footer__btn form-footer__btn-previous  btn btn-smooth nostyle-btn">К загрузке рисунков</button>
-                <button className="form-footer__btn form-footer__btn-result btn btn--disabled nostyle-btn">Узнать результаты</button>
+            <div className="form-footer__buttons">
+                <button className="form-footer__btn form-footer__btn-previous btn btn--previous btn-smooth nostyle-btn" onClick={(e) => { navigate("/testing/1") }}>К загрузке рисунков</button>
+                <button className="form-footer__btn form-footer__btn-result btn btn--result btn--disabled nostyle-btn" onClick={(e) => console.log(new FormData(formRef.current))}>Узнать результаты</button>
             </div>
         </footer>
     </div >;
